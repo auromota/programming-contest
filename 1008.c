d555331af918e91a4cc7e6799a72b0a71c6ab69d
Auro Mota <auro@inatel.br>
2016-05-31 15:09:24 -0300

    1008

---
+++ 1008.c
@@ -0,0 +1,13 @@
+/*
+	Author: Auro Mota <auro@blueorc.com>
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    float c;
+    scanf("%d%d%f", &a, &b, &c);
+    printf("NUMBER = %d\nSALARY = U$ %.2f\n", a, b*c);
+    return 0;
+}
