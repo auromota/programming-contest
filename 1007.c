76b735876991436a89eea5733c3afe6957b361b4
Auro Mota <auro@inatel.br>
2016-05-31 15:09:17 -0300

    1007

---
+++ 1007.c
@@ -0,0 +1,12 @@
+/*
+	Author: Auro Mota <auro@blueorc.com>
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a, b, c, d;
+    scanf("%d%d%d%d", &a, &b, &c, &d);
+    printf("DIFERENCA = %d\n", a*b-c*d);
+    return 0;
+}
