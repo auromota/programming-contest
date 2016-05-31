71273799720952b3ec4a7ae992595991dff84637
Auro Mota <auro@inatel.br>
2016-05-31 16:57:15 -0300

    1930

---
+++ C/1930.c
@@ -0,0 +1,12 @@
+/*
+	Author: Auro Mota <auro@blueorc.com>
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a, b, c, d;
+    scanf("%d%d%d%d", &a, &b, &c, &d);
+    printf("%d\n", a+b+c+d-3);
+    return 0;
+}
