2eff37ef12a467126cc4b54e00bf663a04839f2e
Auro Mota <auro@inatel.br>
2016-05-31 16:54:41 -0300

    1001

---
+++ Java/1001.java
@@ -0,0 +1,16 @@
+/*
+	Author: Auro Mota <auro@blueorc.com>
+*/
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String args[]){
+		Scanner input = new Scanner(System.in);
+		int a = input.nextInt(), b = input.nextInt();
+		a += b;
+		System.out.printf("X = %d\n", a);
+	}
+
+}
