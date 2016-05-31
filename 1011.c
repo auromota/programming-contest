f8d10613f5cf6e61ca4c6abbdec871243257e574
Auro Mota <auro@inatel.br>
2016-05-31 15:12:00 -0300

    1011

---
+++ 1011.c
@@ -0,0 +1,12 @@
+/*
+	Author: Auro Mota <auro@blueorc.com>
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    printf("VOLUME = %.3lf\n", 3.14159*4.0/3.0*a*a*a);
+    return 0;
+}
