b5004824113f0e156a7a3d91307fea6a0a732cc7
Auro Mota <auro@inatel.br>
2016-05-31 17:08:59 -0300

    1984

---
+++ C/1984.c
@@ -0,0 +1,16 @@
+/*
+	Author: Auro Mota <auro@blueorc.com>
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    scanf(" %s", str);
+    for(int i=strlen(str)-1; i>=0; i--) {
+        printf("%c", str[i]);
+    }
+    printf("\n");
+    return 0;
+}
