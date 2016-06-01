a0019ecf7620adb8dca600ff4945a1544cfef136
Auro Mota <auro@inatel.br>
2016-06-01 15:48:06 -0300

    1184

---
+++ C/1184.c
@@ -0,0 +1,22 @@
+/*
+	Author: Auro Mota <auro@blueorc.com>
+*/
+
+#include <stdio.h>
+
+int main() {
+    float a[12][12];
+    char op;
+    int i, j;
+    scanf("%c", &op);
+    float som=0;
+    for(i=0;i<12;i++) {
+        for(j=0;j<12;j++) {
+            scanf("%f",&a[i][j]);
+            if(i>j) som+=a[i][j];
+        }
+    }
+    if(op=='S') printf("%.1f\n", som);
+    else printf("%.1f\n", som/66);
+    return 0;
+}
