ef495ef46b04fe38291e25ac6a8b6cebc2d0d224
Auro Mota <auro@inatel.br>
2016-06-01 15:52:58 -0300

    1143

---
+++ C/1143.c
@@ -0,0 +1,14 @@
+/*
+	Author: Auro Mota <auro@blueorc.com>
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a, i;
+    scanf("%d",&a);
+    for(i=1;i<=a;i++) {
+        printf("%d %d %d\n", i, i*i, i*i*i);
+    }
+    return 0;
+}
