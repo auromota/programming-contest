2cdb810928f1343166d12b4bd6547fca07d2366f
Auro Mota <auro@inatel.br>
2016-06-01 15:42:12 -0300

    1188

---
+++ C/1188.c
@@ -0,0 +1,28 @@
+/*
+	Author: Auro Mota <auro@blueorc.com>
+*/
+
+#include <stdio.h>
+
+int main() {
+    float a[12][12];
+    char op;
+    int i, j;
+    scanf(" %c",&op);
+    float som=0;
+    for(i=0;i<12;i++) {
+        for(j=0;j<12;j++) {
+            scanf("%f",&a[i][j]);
+            if(i>6) {
+                if(j<6) {
+                    if(j>11-i) som+=a[i][j];
+                } else {
+                    if(j<i) som+=a[i][j];
+                }
+            }
+        }
+    }
+    if(op=='S') printf("%.1f\n", som);
+    else printf("%.1f\n", som/30);
+    return 0;
+}
