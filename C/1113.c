7a7f0691d830052cf49afa24696bb1287097ee63
Auro Mota <auro@inatel.br>
2016-06-02 09:16:07 -0300

    1113

---
+++ C/1113.c
@@ -0,0 +1,16 @@
+/*
+	Author: Auro Mota <auro@blueorc.com>
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    while(1) {
+        scanf("%d%d",&a,&b);
+        if(a==b) break;
+        if(a>b) printf("Decrescente\n");
+        else printf("Crescente\n");
+    }
+    return 0;
+}
