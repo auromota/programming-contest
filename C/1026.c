67b65fdb7c85a85f79ccdcd665348a423f0ee4d4
Auro Mota <auro@inatel.br>
2016-06-02 09:27:08 -0300

    1026

---
+++ C/1026.c
@@ -0,0 +1,14 @@
+/*
+	Author: Auro Mota <auro@blueorc.com>
+*/
+
+#include <stdio.h>
+
+int main() {
+    unsigned int a, b, c;
+    while(scanf("%u%u",&a,&b)!=EOF) {
+        c=a^b;
+        printf("%u\n",c);
+    }
+    return 0;
+}
