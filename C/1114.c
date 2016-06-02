1d5e268559eb7f15b31f4b83a6391212c3279350
Auro Mota <auro@inatel.br>
2016-06-02 09:17:46 -0300

    1114

---
+++ C/1114.c
@@ -0,0 +1,18 @@
+/*
+	Author: Auro Mota <auro@blueorc.com>
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a;
+    while(1) {
+        scanf("%d",&a);
+        if(a==2002) {
+        	printf("Acesso Permitido\n");
+            break;
+        }
+        else printf("Senha Invalida\n");
+    }
+    return 0;
+}
