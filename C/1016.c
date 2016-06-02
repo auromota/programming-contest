9dd710032dd607fb15a80dc30cf50180faaf35f3
Auro Mota <auro@inatel.br>
2016-06-02 10:46:12 -0300

    1016

---
+++ C/1016.c
@@ -0,0 +1,13 @@
+/*
+	Author: Auro Mota <auro@blueorc.com>
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int x;
+    scanf("%d", &x);
+    printf("%d minutos\n", 2*x);
+    return 0;
+}
