de6742228d6fac7b199d00f83c94ac8b01d9c9c8
Auro Mota <auro@inatel.br>
2016-06-02 09:28:04 -0300

    1067

---
+++ C/1067.c
@@ -0,0 +1,12 @@
+/*
+	Author: Auro Mota <auro@blueorc.com>
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    for(int i=1;i<=a;i+=2) printf("%d\n", i);
+    return 0;
+}
