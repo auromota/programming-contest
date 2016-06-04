ae8b2b38fa94d0380a2bcd36725a1511215c4485
Auro Mota <palubo01@gmail.com>
2016-06-03 21:03:12 -0300

    1962

--- URI/Solved/Python 3/1962.py
+++ URI/Solved/Python 3/1962.py
@@ -2,8 +2,8 @@ a = int(input())
 while a > 0:
     b = int(input())
     b = 2015-b
-    if b >= 0:
+    if b > 0:
         print(b, 'D.C.')
     else:
-        print(abs(b), 'A.C.')
+        print(abs(b-1), 'A.C.')
     a-=1
